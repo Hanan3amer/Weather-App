32c63f588d8b9ab2961f2264688d7b078edaadad
Hanan3amer <hananamer312003@gmail.com>
2024-06-24 23:53:58 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
 var row = document.getElementById("row")
 
 async function getweather(city) {
-    var url = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e7f7ca52aed445a899c183002241406&q=${city}&days=3`)
-    var res = await url.json()
-    console.log(res)
-    display(res)
+  var url = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e7f7ca52aed445a899c183002241406&q=${city}&days=3`)
+  var res = await url.json()
+  console.log(res)
+  display(res)
 }
 
 function display(res) {
-    var day1 = new Date(res.forecast.forecastday[0].date);
-    var day1date = day1.getDate()
-    var day1name = day1.toLocaleString('en-us', { weekday: 'long' })
-    var day1month = day1.toLocaleString('en-us', { month: 'long' })
-    console.log(`${day1month} ${day1date}`)
+  var day1 = new Date(res.forecast.forecastday[0].date);
+  var day1date = day1.getDate()
+  var day1name = day1.toLocaleString('en-us', { weekday: 'long' })
+  var day1month = day1.toLocaleString('en-us', { month: 'long' })
+  console.log(`${day1month} ${day1date}`)
 
-    var day2 = new Date(res.forecast.forecastday[1].date);
-    var day2date = day2.getDate()
-    var day2name = day2.toLocaleString('en-us', { weekday: 'long' })
-    var day2month = day2.toLocaleString('en-us', { month: 'long' })
-    console.log(`${day2month} ${day2date}`)
+  var day2 = new Date(res.forecast.forecastday[1].date);
+  var day2date = day2.getDate()
+  var day2name = day2.toLocaleString('en-us', { weekday: 'long' })
+  var day2month = day2.toLocaleString('en-us', { month: 'long' })
+  console.log(`${day2month} ${day2date}`)
 
-    var day3 = new Date(res.forecast.forecastday[2].date);
-    var day3date = day3.getDate()
-    var day3name = day3.toLocaleString('en-us', { weekday: 'long' })
-    var day3month = day3.toLocaleString('en-us', { month: 'long' })
+  var day3 = new Date(res.forecast.forecastday[2].date);
+  var day3date = day3.getDate()
+  var day3name = day3.toLocaleString('en-us', { weekday: 'long' })
+  var day3month = day3.toLocaleString('en-us', { month: 'long' })
 
-    console.log(`${day3month} ${day3date}`)
+  console.log(`${day3month} ${day3date}`)
 
-    var box = '';
-    box += `
+  var box = '';
+  box += `
     <div class="col-md-4 my-3">
             <div class="card text-body rounded-4 color-bg">
               <div class="card-header">
@@ -83,7 +83,7 @@ function display(res) {
               </div>
             </div>
           </div>
-        <div class="col-md-4">
+        <div class="col-md-4 my-3">
             <div class="card text-body rounded-4 color-bg">
               <div class="card-header">
                 <div class="d-flex">
@@ -106,33 +106,33 @@ function display(res) {
             </div>
           </div>
 `
-    row.innerHTML = box
+  row.innerHTML = box
 }
 
 var search = document.getElementById("search")
 search.addEventListener('keyup', function () {
-    var city = search.value.trim()
-    getweather(city)
+  var city = search.value.trim()
+  getweather(city)
 
 })
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        getweather(`${latitude},${longitude}`)
-    },
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    getweather(`${latitude},${longitude}`)
+  },
 
-        function (error) {
-            console.log(error, "there is no location found")
-            getweather("Cairo")
-        }
-    )
+    function (error) {
+      console.log(error, "there is no location found")
+      getweather("Cairo")
+    }
+  )
 } else {
-    getweather("Cairo")
+  getweather("Cairo")
 }
 var findBtn = document.getElementById("findBtn")
 findBtn.addEventListener('click', function () {
-    var city = search.value.trim()
-    getweather(city)
+  var city = search.value.trim()
+  getweather(city)
 })
